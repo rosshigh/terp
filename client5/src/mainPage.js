5364522db3f8e0f081ec9c77f560b6564133e5d4
rosshigh <hightowe@uwm.edu>
2018-05-27 09:13:07 -0500

    just quiz tool

--- client5/src/mainPage.js
+++ client5/src/mainPage.js
@@ -13,13 +13,13 @@ export class MainPage{
     
     configureRouter(config, router) {
         config.map([
-            { route: ['home',''],     name: 'home',       moduleId: 'home' },
-            { route: 'Schedule',        name: 'schedule',    moduleId: 'schedule',   nav: true,    title: 'Schedule', settings: { auth: true, roles: 'user' } },
-            { route: 'Resources',      name: 'resources',  moduleId: 'resources', nav: true,    title: 'Resources', settings: { auth: true, roles:'user' } },
-            { route: 'Videos',      name: 'videos',  moduleId: 'videos', nav: true,    title: 'Videos', settings: { auth: true, roles: 'user' } },
-            { route: 'QuizTool',      name: 'quiz',  moduleId: 'quizTool', nav: true,    title: 'Quiz Tool', settings: { auth: true, roles: 'user' } },
-            { route: 'Admin',      name: 'admin',  moduleId: 'admin', nav: true,    title: 'Admin', settings: { auth: true, roles: 'admin' } },
-            { route: 'login',     name: 'login',       moduleId: './login' },
+            // { route: ['home',''],     name: 'home',       moduleId: 'home' },
+            // { route: 'Schedule',        name: 'schedule',    moduleId: 'schedule',   nav: true,    title: 'Schedule', settings: { auth: true, roles: 'user' } },
+            // { route: 'Resources',      name: 'resources',  moduleId: 'resources', nav: true,    title: 'Resources', settings: { auth: true, roles:'user' } },
+            // { route: 'Videos',      name: 'videos',  moduleId: 'videos', nav: true,    title: 'Videos', settings: { auth: true, roles: 'user' } },
+            { route: ['QuizTool',''],      name: 'quiz',  moduleId: 'quizTool', nav: true,    title: 'Quiz Tool', settings: { auth: true, roles: 'user' } },
+            // { route: 'Admin',      name: 'admin',  moduleId: 'admin', nav: true,    title: 'Admin', settings: { auth: true, roles: 'admin' } },
+            // { route: 'login',     name: 'login',       moduleId: './login' },
         ]);
 
         this.router = router;
